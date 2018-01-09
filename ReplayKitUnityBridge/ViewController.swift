90b2ede25c3b9f823d8c6a186e4e925f0d6b8a29
Sonam <sonam@us2-nyc-sonam-2.local>
2018-01-09 16:04:29 -0500

    Initial Commit

---
+++ ReplayKitUnityBridge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReplayKitUnityBridge
+//
+//  Created by Sonam on 1/9/18.
+//  Copyright © 2018 Sonam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
