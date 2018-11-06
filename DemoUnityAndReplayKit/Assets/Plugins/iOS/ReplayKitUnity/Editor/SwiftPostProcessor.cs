b3b80553c1a5c61cd1c60a2bc4145644bbcccadf
Troy <troy.lamerton@gmail.com>
2018-11-06 15:49:20 +0100

    define ios build settings in unity

--- DemoUnityAndReplayKit/Assets/Plugins/iOS/ReplayKitUnity/Editor/SwiftPostProcessor.cs
+++ DemoUnityAndReplayKit/Assets/Plugins/iOS/ReplayKitUnity/Editor/SwiftPostProcessor.cs
@@ -14,25 +14,38 @@ using System.Linq;
 /// </summary> 
 
 public static class SwiftPostProcessor {
+
+        // Name of folder (iOS/<folder>) and the file name prefix
+        private static string pluginName = "ReplayKitUnity";
+
         [PostProcessBuild]
         public static void OnPostProcessBuild(BuildTarget buildTarget, string buildPath) {
             if(buildTarget == BuildTarget.iOS) {
 
-                // We need to tell the Unity build to look at the write build file path and specifically reference the exposed Swift header file for it to work 
+                // We need to tell the Unity build to look at the write build file path and specifically reference the exposed Swift header file for it to work
                 var projPath = buildPath + "/Unity-iPhone.xcodeproj/project.pbxproj";
                 var proj = new PBXProject();
                 proj.ReadFromFile(projPath);
 
                 var targetGuid = proj.TargetGuidByName(PBXProject.GetUnityTargetName());
 
+                proj.AddBuildProperty(targetGuid, "SWIFT_VERSION", "4.0");
+
                 proj.SetBuildProperty(targetGuid, "ENABLE_BITCODE", "NO");
 
-                // This must match the file path of where the bridging header lives in your Unity project 
-                proj.SetBuildProperty(targetGuid, "SWIFT_OBJC_BRIDGING_HEADER", "Libraries/Plugins/iOS/ReplayKitUnity/Source/ReplayKitUnityBridge-Bridging-Header.h");
+                // This must match the file path of where the bridging header lives in your Unity project
+                proj.SetBuildProperty(targetGuid, "SWIFT_OBJC_BRIDGING_HEADER", "Libraries/Plugins/iOS/"+pluginName+"/Source/"+pluginName+"Bridge-Bridging-Header.h");
 
-                // We specifically reference the generated Swift to Objective-C header 
-                proj.SetBuildProperty(targetGuid, "SWIFT_OBJC_INTERFACE_HEADER_NAME", "ReplayKitUnityBridge-Swift.h");
+                // We specifically reference the generated Swift to Objective-C header
+                proj.SetBuildProperty(targetGuid, "SWIFT_OBJC_INTERFACE_HEADER_NAME", pluginName+"Bridge-Swift.h");
+                proj.SetBuildProperty(targetGuid, "SWT_OBJC_INTERFACE_HEADER_NAME", pluginName+"Bridge-Swift.h");
+
                 proj.AddBuildProperty(targetGuid, "LD_RUNPATH_SEARCH_PATHS", "@executable_path/Frameworks");
+
+
+                // Use below code if external frameworks are needed
+                // proj.SetBuildProperty(targetGuid, "FRAMEWORK_SEARCH_PATHS", "$(inherited)");
+                proj.AddBuildProperty(targetGuid, "FRAMEWORK_SEARCH_PATHS", "$(PROJECT_DIR)/Frameworks");
 
                 proj.WriteToFile(projPath);
             }
